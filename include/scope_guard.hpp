186efe05d49879df10631bcd401b8568e18989fd
Christoph Conrads <christoph.conrads@deepstreamhub.com>
2017-01-10 13:41:46 +0000

    Parser: replace BOOST_SCOPE_EXIT with Scope Guard
    Replace BOOST_SCOPE_EXIT with C++11 Scope Guards (see scope_guard.hpp
    for a reference). BOOST_SCOPE_EXIT causes compiler warnings and
    generates huge amounts of code. For educational purposes, you can find
    the preprocessed code for the following code snippet below:
    	BOOST_SCOPE_EXIT(tokenizing_header_) {
    		tokenizing_header_ = true;
    	} BOOST_SCOPE_EXIT_END
    
     typedef void (*boost_se_tag_0_114)(int tokenizing_header_); typedef __typeof__(boost::type_of::ensure_obj(::boost::scope_exit::detail::wrap( ::boost::scope_exit::detail::deref(tokenizing_header_, static_cast<boost_se_tag_0_114>(0))))) boost_se_wrapped_t_0_114 ; typedef boost_se_wrapped_t_0_114::type boost_se_capture_t_0_114 ; struct boost_se_params_t_114 { typedef boost_se_capture_t_0_114 boost_se_param_t_0_114 ; ::boost::scope_exit::detail::member< boost_se_param_t_0_114, boost_se_tag_0_114 > boost_se_param_0_114; } boost_se_params_114 = { { ::boost::scope_exit::detail::deref(tokenizing_header_, static_cast<boost_se_tag_0_114>(0)) } } ; ::boost::scope_exit::detail::declared< ::boost::scope_exit::detail::resolve< sizeof(boost_scope_exit_aux_args) >::cmp1<0>::cmp2 > boost_scope_exit_aux_args; boost_scope_exit_aux_args.value = &boost_se_params_114; struct boost_se_guard_t_114 { boost_se_params_t_114* boost_se_params_; boost_se_guard_t_114 (void* boost_se_params) : boost_se_params_( (boost_se_params_t_114*)boost_se_params) {} ~boost_se_guard_t_114() { boost_se_body( boost_se_params_->boost_se_param_0_114.value ); } static void boost_se_body( boost_se_params_t_114:: boost_se_param_t_0_114 tokenizing_header_ ) {
      tokenizing_header_ = true;
     } } boost_se_guard_116(boost_scope_exit_aux_args.value);

---
+++ include/scope_guard.hpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2016-2017 deepstreamHub GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef DEEPSTREAM_SCOPEGUARD_HPP
+#define DEEPSTREAM_SCOPEGUARD_HPP
+
+#include <functional>
+
+#include <boost/preprocessor/cat.hpp>
+
+#include <use.hpp>
+
+
+namespace deepstream
+{
+	// A. Alexandrescu, P. Marginean:
+	// "Generic: Change the Way You Write Exception-Safe Code -- Forever"
+	// Dr. Dobbs Journal, 2000
+	// www.drdobbs.com/cpp/generic-change-the-way-you-write-excepti/184403758
+	struct ScopeGuard
+	{
+		typedef std::function<void(void)> FunT;
+
+		ScopeGuard(FunT f) : f_(f) {};
+		~ScopeGuard() { f_(); }
+
+		FunT f_;
+	};
+}
+
+
+#define DEEPSTREAM_ON_EXIT(...) \
+	const ::deepstream::ScopeGuard BOOST_PP_CAT(deepstream_guard,__LINE__)(__VA_ARGS__); \
+	::deepstream::use( BOOST_PP_CAT(deepstream_guard, __LINE__) )
+
+#endif
