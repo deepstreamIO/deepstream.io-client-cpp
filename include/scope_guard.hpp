9a9f82cb3d54dfe144b13f222c42e041b0f6d6f9
Christoph Conrads <christoph.conrads@deepstreamhub.com>
2017-01-12 19:30:25 +0000

    Fix year in copyright notice

--- include/scope_guard.hpp
+++ include/scope_guard.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2017 deepstreamHub GmbH
+ * Copyright 2017 deepstreamHub GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
