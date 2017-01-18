4d10d4120404fe93881c82d6328309ae67d724d4
Christoph Conrads <christoph.conrads@deepstreamhub.com>
2017-01-18 13:18:29 +0000

    Src: draft interface for client code
    Does not compile.

--- include/client.hpp
+++ include/client.hpp
@@ -16,13 +16,21 @@
 #ifndef DEEPSTREAM_CLIENT_HPP
 #define DEEPSTREAM_CLIENT_HPP
 
+#include <functional>
 #include <iosfwd>
+#include <list>
+#include <vector>
+
+#include <time.hpp>
 
 
 namespace deepstream
 {
+	enum class Topic;
+	enum class Action;
 	enum class Sender;
 	struct Message;
+
 
 	namespace client
 	{
@@ -38,10 +46,84 @@ namespace deepstream
 			DISCONNECTED
 		};
 
+		enum class Error
+		{
+			NONE,
+			INVALID_STATE_TRANSITION,
+			TIMEOUT,
+			UNKNOWN_REQUEST
+		};
+
 		std::ostream& operator<<(std::ostream&, State);
 
 
-		State transition(State s, const Message& message, Sender sender);
+		State transition(State, const Message&, Sender);
+
+
+		struct Request
+		{
+			typedef std::vector<char> Pattern;
+
+
+			explicit Request(Topic, Action, const Pattern& pattern);
+
+			Topic topic() const { return topic_; }
+			Action action() const { return action_; }
+			const Pattern& pattern() const { return pattern_; }
+
+
+			const Topic topic_;
+			const Action action_;
+			const Pattern pattern_;
+		};
+
+
+		struct Timer;
+
+
+		struct Client
+		{
+			typedef std::list<Request> RequestList;
+			typedef RequestList::iterator Key;
+
+			static_assert( sizeof(Key) <= sizeof(void*), "" );
+
+
+			explicit Client(Timer*);
+			virtual ~Client();
+
+			State send(const Message&);
+			State receive(const Message&);
+			State notify(Key);
+
+			State handle_error_(const Message&, Sender, Error);
+			State handle_timeout_(Key);
+
+			virtual State handle_error_impl_(const Message&, Sender, Error) = 0;
+			virtual State handle_timeout_impl_(Key) = 0;
+
+
+			State state_;
+			RequestList requests_;
+
+			Timer* const p_timer_;
+		};
+
+
+		struct Timer
+		{
+			typedef State Return;
+			typedef Client::Key Argument;
+			typedef std::function<Return(Argument)> Callback;
+
+			virtual ~Timer() {}
+
+			void set(time::Duration, Callback, const Argument&);
+			void unset(const Argument&);
+
+			virtual void set_impl_(time::Duration, Callback, const Argument&)=0;
+			virtual void unset_impl_(const Argument&) = 0;
+		};
 	}
 }
 
