f7bab6ea3eae374539c89cdc02c61fdddfdb39e9
Christoph Conrads <christoph.conrads@deepstreamhub.com>
2017-01-19 14:41:55 +0000

    Demo: add POCO C++ Library WebSockets client demo

---
+++ demo/poco-websockets/FindPOCO.cmake
@@ -0,0 +1,29 @@
+# Copyright 2017 deepstreamHub GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+find_path(POCO_INCLUDE_DIR NAMES Poco/Poco.h)
+find_library(POCO_LIBRARY NAMES PocoFoundation)
+find_library(POCO_NET_LIBRARY NAMES PocoNet)
+
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+	POCO REQUIRED_VARS POCO_LIBRARY POCO_INCLUDE_DIR)
+
+if(POCO_FOUND)
+	set(POCO_LIBRARIES ${POCO_LIBRARY} ${POCO_NET_LIBRARY})
+	set(POCO_INCLUDE_DIRS ${POCO_INCLUDE_DIR})
+endif()
+
+mark_as_advanced(POCO_INCLUDE_DIR POCO_LIBRARY)
---
+++ demo/poco-websockets/README.md
@@ -0,0 +1,13 @@
+# POCO C++ libraries WebSockets demo
+
+Data as of Jan 19, 2017.
+
+## Weaknesses and Problems
+
+- not available in `yum` on Amazon Linux AMI
+- v1.3.6, released on Jan 15, 2010, available in `apt-get` on Raspbian
+
+
+## Strengths
+
+- builds with CMake 2.8.12 like the deepstream.io C++ libraries
