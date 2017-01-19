4d2a5e58fa38e7ae7edd6bdef49be46b034f5946
Christoph Conrads <christoph.conrads@deepstreamhub.com>
2017-01-19 10:24:48 +0000

    Revert "Remove Bison demo"
    This reverts commit ce342c2ae0f4b4740e7ba7e4483fe3b8d5cb6643.

---
+++ bison-demo/main.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "parser.h"
+#include "parser.tab.h"
+
+#include <assert.h>
+
+
+int yylex(void* lvalp, YYLTYPE* p_loc, struct state* p_state)
+{
+	(void) lvalp;
+
+	assert( p_state );
+
+	const char* const first = p_state->first;
+	const char* const last = p_state->last;
+	const char* p = p_state->p;
+	const char* const p_save = p;
+
+	assert( first );
+	assert( last );
+	assert( p >= first );
+
+    if(p == last)
+        return EOF;
+
+	//printf("%c (%d)\n", *p_save, *p_save);
+
+	if(*p == '\x1e' || *p == '\x1f')
+	{
+		p_state->p = p + 1;
+		p_loc->first_column = p_save - first + 1;
+		p_loc->last_column =  p_save - first + 1;
+
+		return *p;
+	}
+
+	while(p != last && *p != '\x1e' && *p != '\x1f')
+		++p;
+
+	p_state->p = p;
+	size_t matchlen = p - p_save;
+
+	p_loc->first_column = p_save - first + 1;
+	p_loc->last_column = p - first;
+
+	//printf("%d:%d %zu\n", p_loc->first_column, p_loc->last_column, matchlen);
+
+	if( !p_state->tokenize_header )
+		return NON_SEPARATOR;
+
+	if( !strncmp("E", p_save, matchlen) )
+		return TOPIC_EVENT;
+	if( !strncmp("R", p_save, matchlen) )
+		return TOPIC_RECORD;
+	if( !strncmp("S", p_save, matchlen) )
+		return ACTION_SUBSCRIBE;
+	if( !strncmp("A", p_save, matchlen) )
+		return ACK;
+
+	return TOPIC_UNKNOWN;
+}
+
+
+void yyerror(const YYLTYPE* p_loc, const struct state* p_state, const char* str)
+{
+	(void) p_state;
+	(void) str;
+
+	fprintf(
+		stderr, "error %d bytes into the message\n",
+		p_loc->first_column);
+}
+
+
+int main()
+{
+    char input[] = "E|A|S|foo|bar|baz+";
+	const char* const first = input;
+	const char* const last = input + strlen(input);
+
+	for(char* p = input; p != last; ++p)
+	{
+		if(*p == '+') *p = '\x1e';
+		if(*p == '|') *p = '\x1f';
+	}
+
+	for(size_t i = 0; i < 1u<<20; ++i)
+	{
+		struct state state = { first, last, true, first, 0 };
+		int ret = yyparse(&state);
+	}
+
+    //printf("ret = %d args=%zu\n", ret, state.num_data);
+}
