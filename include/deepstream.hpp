d4771f9b8f7d0e81c32df4e2cd592acdca942928
Christoph Conrads <christoph.conrads@deepstreamhub.com>
2017-02-02 20:22:55 +0100

    Client: implement event loop
    This commit does not link successfully because `Event::notify_` has yet
    to be implemented.

--- include/deepstream.hpp
+++ include/deepstream.hpp
@@ -22,6 +22,7 @@
 #include <memory>
 #include <string>
 
+#include <event.hpp>
 // 201701227:
 // we have to include `parser.hpp` because `parser::MessageList`,
 // `parser::ErrorList` are `typedef`s and cannot be forward declared.
@@ -72,6 +73,13 @@ namespace deepstream
 		void close();
 
 		client::State getConnectionState() { return state_; }
+
+
+		void process_messages();
+
+
+		Event event;
+
 
 		/**
 		 * This method reads the next frame from the websocket and extracts its
