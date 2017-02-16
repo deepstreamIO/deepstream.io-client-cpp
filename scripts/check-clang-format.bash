e2599fc1581aab8f77b13b27af1da1d32620a16c
Andrew McDermott <aim@frobware.com>
2017-02-16 09:51:38 +0000

    scripts/check-clang-format: skip generated files
    Don't check CMake generated sources, or any of the flex generated sources.
    
    Signed-off-by: Andrew McDermott <aim@frobware.com>

--- scripts/check-clang-format.bash
+++ scripts/check-clang-format.bash
@@ -11,10 +11,13 @@ type -p clang-format > /dev/null 2>&1 || { echo "error: clang-format is not inst
 [ "${VERBOSE:-0}" = "1" ] && echo "verifying coding style with clang-format..."
 
 for i in $(find * -name '*.cpp' -or -name '*.hpp' -or -name '*.h' -or -name '*.c'); do
+    [[ "$i" =~ "lexer" ]] && continue
+    [[ "$i" =~ "CMakeFiles" ]] && continue
+    [[ "${VERBOSE:-0}" = "1" ]] && echo "verifying coding style on $i"
     diff -u <(cat $i) <(clang-format --style=file $i) > /dev/null
     if [ $? -ne 0 ]; then
-	echo "error: clang-format is sad: $i is not compliant." >&2
-	status=1
+        echo "error: clang-format is sad: $i is not compliant." >&2
+        status=1
     fi
 done
 
