bd0153f2a5371ccaeb2a36f79b4552fde184be37
Andrew McDermott <aim@frobware.com>
2017-02-17 11:36:47 +0000

    Rename coverage info file

--- lcov.bash
+++ lcov.bash
@@ -4,4 +4,5 @@ lcov --directory . --zerocounters
 make test
 lcov --directory . --capture --output-file coverage.info
 lcov --remove coverage.info 'tests/*' '/usr/*' --output-file coverage.final
-genhtml -o coverage.html coverage.final
+mv coverage.final coverage.info
+genhtml -o coveragereport coverage.info
