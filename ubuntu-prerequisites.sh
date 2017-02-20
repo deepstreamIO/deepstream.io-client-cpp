4240bb9431b87f6911bb4759cb78a68fe6b3402b
Andrew McDermott <frobware@users.noreply.github.com>
2017-02-20 15:17:40 +0000

    Use CMake externalproject_add() to build poco dependency (#65)

---
+++ ubuntu-prerequisites.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+apt update -yq
+
+apt-get -yq --no-install-suggests --no-install-recommends install \
+	build-essential \
+	cmake \
+	curl \
+	flex \
+	gcc \
+	lcov \
+	libboost-dev \
+	libboost-test-dev \
+	valgrind \
+	wget
