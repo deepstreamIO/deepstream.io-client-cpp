cd4534ee267bd06944dcce1fe40ee662351eb114
Andrew McDermott <aim@frobware.com>
2017-02-27 10:50:09 +0000

    Use ValueError for Swig Python test

--- test/python-swig-test.py
+++ test/python-swig-test.py
@@ -5,7 +5,7 @@ import deepstream
 version = deepstream.version_to_string()
 
 if deepstream.version_to_string() != "0.1.0":
-    raise Exception("Version mismatch; expected 0.0.1, got {}".format(version))
+    raise ValueError("Version mismatch; expected 0.0.1, got {}".format(version))
 
 
 
