6ba9539a17543297ec99b360ad3c7dfa9496f265
Andrew McDermott <aim@frobware.com>
2017-02-28 08:48:19 +0000

    update prerequisites for trusty

--- ubuntu-prerequisites.sh
+++ ubuntu-prerequisites.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+if [ "$(lsb_release -cs)" = "trusty" ]; then
+    apt-add-repository "deb http://archive.ubuntu.com/ubuntu trusty-backports main restricted universe multiverse"
+fi
+
 apt update -yq
 
 apt-get -yq --no-install-suggests --no-install-recommends install \
 	build-essential \
-	cmake \
-	curl \
 	flex \
 	gcc \
 	lcov \
@@ -13,7 +15,14 @@ apt-get -yq --no-install-suggests --no-install-recommends install \
 	libboost-test-dev \
 	libssl-dev \
 	python3 \
-	python3-dev \
-	swig \
+        python3-dev \
 	valgrind \
 	wget
+
+if [ "$(lsb_release -cs)" = "trusty" ]; then
+    apt-get -yq -t trusty-backports install swig3.0
+    apt-get -yq -t trusty-backports install cmake
+else
+    apt-get -yq install swig3.0
+    apt-get -yq install cmake
+fi
