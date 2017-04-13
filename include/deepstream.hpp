5f824ef5cf70c18d34df59d1c703b575a9e05e2a
John Diamond <johndiamond@gmx.co.uk>
2017-04-13 21:48:40 +0200

    feat: serialization + login wrapper (partial/doesn't build)

--- include/deepstream.hpp
+++ include/deepstream.hpp
@@ -27,6 +27,7 @@
 #include <deepstream/lib/json.hpp>
 
 #include <string>
+#include <exception>
 
 namespace deepstream {
 
@@ -38,7 +39,7 @@ namespace deepstream {
             : wsh_()
             , error_handler_()
             , client_(uri, wsh_, error_handler_)
-            , event(client_)
+            , event(client_, error_handler_)
         {}
 
         /**
@@ -101,65 +102,131 @@ namespace deepstream {
 
         struct Event {
             typedef std::function<void(const json&)> SubscribeFn;
-            typedef deepstream::Event::SubscriptionId SubscriptionId;
+            typedef std::map<SubscriptionId, SubscribeFn> SubscriptionMap;
+            typedef std::function<bool(const std::string&, bool)> ListenFn;
 
-            Event(Client &client)
+            Event(Client &client, ErrorHandler &error_handler)
                 : client_(client)
+                , error_handler_(error_handler)
             {}
 
             void emit(std::string name, json data) {
-                std::string data_str = "O" + data.dump();
-                client_.event.emit(Buffer(name.cbegin(), name.cend()), Buffer(data_str.cbegin(), data_str.cend()));
+                const Buffer &data_buff = to_prefixed_buffer(data);
+                const Buffer name_buff(name.cbegin(), name.cend());
+                client_.event.emit(name_buff, data_buff);
             }
 
             SubscriptionId subscribe(std::string name, SubscribeFn callback) {
-                deepstream::Event::SubscribeFn core_callback([=](const Buffer &data){
-                        callback(json::parse(data));
+                Buffer name_buff(name.cbegin(), name.cend());
+                deepstream::Event::SubscribeFn core_callback([&this, callback](const Buffer &prefixed_buff){
+                        const json &data = prefixed_to_json(prefixed_buff);
+                        callback(data);
                         });
-                return client_.event.subscribe(Buffer(name.cbegin(), name.cend()), callback);
+                SubscriptionId subscription_id =
+                    client_.event.subscribe(name_buff, core_callback);
+                subscription_map_[subscription_id] = callback;
+                return subscription_id;
             }
 
-            void unsubscribe(std::string name, SubscriptionId id) {
-                client_.event.unsubscribe(Buffer(name.cbegin(), name.cend()), id);
+            void unsubscribe(std::string name, SubscriptionId subscription_id) {
+                Buffer name_buff(name.cbegin(), name.cend());
+                client_.event.unsubscribe(name_buff, subscription_id);
             }
 
+            void listen(const std::string &pattern, const ListenFn callback) {
+                using Core = deepstream::Event;
+                Core::ListenFn core_callback([callback](const Core::Name &match, bool is_subscribed) -> bool{
+                        std::string pattern_str(match.data(), match.size());
+                        return callback(pattern_str, is_subscribed);
+                        });
+                Core::Name pattern_buff(pattern.cbegin(), pattern.cend());
+                client_.event.listen(pattern_buff, core_callback);
+            }
+
+            void unlisten(const std::string &pattern) {
+                Buffer pattern_buff(pattern.cbegin(), pattern.cend());
+                client_.event.unlisten(pattern_buff);
+            }
+
+            SubscriptionMap subscription_map_;
             Client &client_;
+            ErrorHandler &error_handler_;
         };
         struct Presence {
             Presence() = default;
         };
 
-        Buffer to_buffer(json &data);
+        Buffer to_buffer(const json &);
 
-        Buffer to_buffer_prefixed(json &data);
+        Buffer to_prefixed_buffer(const json &);
 
-        json to_json(Buffer &data);
+        json to_json(const Buffer &);
 
-        json prefixed_to_json(Buffer &data);
+        json prefixed_to_json(const Buffer &);
 
     public:
         Event event;
         Presence presence;
     };
 
-    Buffer Deepstream::to_buffer(json &data) {
+    Buffer Deepstream::to_buffer(const json &data) {
 
         return Buffer();
     }
 
-    Buffer Deepstream::to_buffer_prefixed(json &data) {
-        return Buffer();
+    enum class DSType : char {
+        STRING = 'S',
+        OBJECT = 'O',
+        NUMBER = 'N',
+        NULL_ = 'L',
+        TRUE = 'T',
+        FALSE = 'F',
+        UNDEFINED = 'U'
+    };
+
+    Buffer Deepstream::to_prefixed_buffer(const json &data) {
+        DSType prefix;
+
+        if (data.is_string()) {
+            prefix = DSType::STRING;
+        } else if (data.is_object()) {
+            prefix = DSType::OBJECT;
+        } else if (data.is_number()) {
+            prefix = DSType::NUMBER;
+        } else if (data.is_null()) {
+            prefix = DSType::NULL_;
+        } else if (data.is_boolean()) {
+            prefix = data ? DSType::TRUE : DSType::FALSE;
+        } else if (false /* undefined isn't in the JSON spec */){
+            prefix = DSType::UNDEFINED;
+        } else {
+            throw new std::runtime_error("Unable to serialize type: " + data.dump());
+        }
+
+        if (prefix == DSType::STRING
+                || prefix == DSType::OBJECT
+                || prefix == DSType::NUMBER) {
+            std::string str = data.dump();
+            str.insert(0, 1, static_cast<char>(prefix));
+            return Buffer(str);
+        }
+
+        return Buffer{ static_cast<char>(prefix) };
     }
 
-    void to_json(json &j, const Buffer &buff) {
+    json to_json(const Buffer &buff) {
+        return json({});
+    }
+
+    // void to_json(json &j, const Buffer &buff) { };
+    // void from_json(const json &j, Buffer &buff) { return json({}); }
+
+    json Deepstream::prefixed_to_json(const Buffer &buff) {
         switch (buff[0]) {
             case 'S':
-                {
-                    j = json(std::string(buff.data()));
-                } break;
+                return std::string(buff.data() + 1, buff.size() - 1);
             case 'O':
                 {
-
                 } break;
             case 'N':
                 {
@@ -182,15 +249,8 @@ namespace deepstream {
 
                 } break;
         }
-        return json({});
-    }
-
-    void from_json(const json &j, Buffer &buff) {
-        return json({});
-    }
-
-    json Deepstream::prefixed_to_json(Buffer &data) {
-        return json({});
+        error_handler_.on_error("Unable to deserialize buffer: "
+                + std::string(buff, buff.size());
     }
 
     //struct Deepstream::Presence {
